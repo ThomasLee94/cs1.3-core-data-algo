d644ba006621a102a35fddb7ee5f06ccd1cb01b8
Thomas <thomaslee9474@gmail.com>
2019-06-06 11:34:21 -0700

    [changes]

--- Code/search.py
+++ Code/search.py
@@ -21,11 +21,10 @@ def linear_search_recursive(array: [int, str], item: (int, str), index=0) -> (in
     if index >= len(array):
         return None
 
-    # case: item is found, return index
     if array[index] == item:
         return index
     
-    # case: recursive call, increment index until item is found
+    # case: increment index until item is found
     return linear_search_recursive(array, item, index + 1)
 
 
@@ -45,6 +44,7 @@ def binary_search_iterative(array: [int, str], item: (int, str)) -> (int, None):
     left_index = 0
     right_index= len(array) - 1
     
+    # left_index should never == right_index
     while left_index != right_index:
         # middle_index must be based of update left and right
         middle_index = (left_index + right_index) // 2
@@ -74,18 +74,18 @@ def binary_search_recursive(array: [int, str], item: (int, str), left_index=0, r
     if right_index == None:
         right_index = len(array) - 1 
 
-    # middle_index must be based of update left and right
+    # middle_index must be based of update left and right (floor)
     middle_index = (left_index + right_index) // 2
-
-    if item == array[middle_index]:
-        return middle_index
     
     # case: item does not exist in list
     if left_index >= right_index:
         return None
     
+    if item == array[middle_index]:
+        return middle_index
+    
     # case: middle_index <= item <= right (RHS)
-    # +1 is needed to update left_index as we are flooring, for the case for 2 elements in a list
+    # +1 is needed to update left_index as we are flooring, for case: 2 elements in a list
     if item > array[middle_index]:
         return binary_search_recursive(array, item, middle_index + 1, right_index)
     # case: left <= item <= middle_index (LHS)
